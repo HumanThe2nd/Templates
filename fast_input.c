7f4240b9192bbdde3817d01fdd9370af978c8f21
Dan Shan <96434604+HumanThe2nd@users.noreply.github.com>
2024-03-16 21:07:14 -0400

    Create fast_input.c

---
+++ fast_input.c
@@ -0,0 +1,15 @@
+// scans an integer until a non-numeric character is reached
+int scan() {
+    int num = 0;
+    char c = getchar_unlocked();
+    while (c>'9'||c<'0') c = getchar_unlocked();
+    while (c>='0'&&c<='9') {
+        num=num*10+c-'0';
+        c=getchar_unlocked();
+    }
+    return num;
+}
+// reference
+int main(){
+int a = scan();
+}
