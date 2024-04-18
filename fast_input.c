f8a4f26b210aa8ca3d5a35efbb5f7b130bc30250
Dan Shan <96434604+HumanThe2nd@users.noreply.github.com>
2024-04-17 20:53:02 -0400

    Update fast_input.c

--- fast_input.c
+++ fast_input.c
@@ -9,7 +9,16 @@ int scan() {
     }
     return num;
 }
+// accounts for negatives
+int nscan() { // scans negatives. 
+    int n = 0, a = 0;
+    char c = getchar_unlocked();
+    if (c == '-') n = 1, c = getchar_unlocked();
+    while('0' <= c && c <= '9') a = a * 10 + c - '0', c = getchar_unlocked();
+    return n ? -a : a;
+}
 // reference
 int main(){
 int a = scan();
+int b = nscan();
 }
