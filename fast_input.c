2af7ff7ef514efbfe9fbadbca48dd0bb50d2ad52
Dan Shan <96434604+HumanThe2nd@users.noreply.github.com>
2024-04-29 17:45:30 -0400

    Update fast_input.c

--- fast_input.c
+++ fast_input.c
@@ -1,6 +1,7 @@
 // macro scan
-#define mscan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0)
-// scans an integer until a non-numeric character is reached
+#define mscan(x) do{while((x=getchar_unlocked())<'0'); for(x-='0'; '0'<=(_=getchar_unlocked()); x=(x<<3)+(x<<1)+_-'0');}while(0)
+char _;
+// method scanner
 int scan() {
     int num = 0;
     char c = getchar_unlocked();
@@ -11,8 +12,8 @@ int scan() {
     }
     return num;
 }
-// accounts for negatives
-int nscan() { // scans negatives. 
+// method scanner with negatives
+int nscan() {
     int n = 0, a = 0;
     char c = getchar_unlocked();
     if (c == '-') n = 1, c = getchar_unlocked();
@@ -23,4 +24,6 @@ int nscan() { // scans negatives.
 int main(){
 int a = scan();
 int b = nscan();
+int c;
+scan(c);
 }
