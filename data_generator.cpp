7bfe3115cf0951fe8e6863b23a338bae6b7e1970
Dan Shan <96434604+HumanThe2nd@users.noreply.github.com>
2024-07-25 16:46:29 -0400

    Create data_generator.cpp

---
+++ data_generator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <random>
+int main(int argc, char** argv) {
+    const int suite=atoi(argv[1],seed=atoi(argv[2]); // choose seed based on case number
+    std::mt19937 gen(seed);  // Use the provided seed
+    std::uniform_int_distribution<> dis(1,100); // generator between 1 to 100 inclusive
+    int k=dis(gen),n=dis(gen),l=dis(gen),m=0;
+    if(suite==1){ // batch 1
+        // batch 1 body
+        return 0;
+    }
+    if(suite==2){ // batch 2
+        // batch 2 body
+       return 0;
+    }
+    // other batches
+}
