d9f536272498ce91a33ac4d371855f94b9c192c0
Dan Shan <96434604+HumanThe2nd@users.noreply.github.com>
2024-09-06 23:50:25 -0400

    Create Fibonacci.c

---
+++ Fibonacci.c
@@ -0,0 +1,16 @@
+// Fibonnaci
+// Naive recursion approach to computing fibonnaci numbers
+// Author: Dan Shan
+// Date: 2024-09-06
+// Naive solution for fibonacci sequence
+#include <stdio.h>
+int fib(int n) {
+    if(n==2||n==1) return n-1;
+    return fib(n-1)+fib(n-2);
+}
+int main() {
+    int n;
+    scanf("%d", &n);
+    printf("%d\n", fib(n));
+    return 0;
+}
