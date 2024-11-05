a951f45e1ed2cd7bdf2416ecd839d8c2d2c4d0d9
Human <96434604+HumanThe2nd@users.noreply.github.com>
2024-11-05 12:50:25 -0500

    Create prime_factorization.c

---
+++ prime_factorization.c
@@ -0,0 +1,13 @@
+long long primefac(long long n){
+	long long m=-1;
+	while(!(n&1)){
+		m=2;
+		n>>=1;
+	}
+	for(int i=3;i<=sqrt(n);i+=2){
+		while(n%i==0){
+			m=i;
+			n/=i;
+		}
+	}
+	if(n>m) m=n; // remainder could also be a larger prime
