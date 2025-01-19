a39475124c70e51471ff8de1060a18efb074b44a
Human <96434604+HumanThe2nd@users.noreply.github.com>
2025-01-18 21:28:53 -0500

    Update fast_input.c

--- fast_input.c
+++ fast_input.c
@@ -1,7 +1,30 @@
+/*
+Fast Input templates
+Author: Dan Shan
+Last Updated: 2025-01-18
+*/
+
 // macro faster for small inputs and methods faster for larger inputs theoretically
 // macro scan
 #define mscan(x) do{while((x=getchar_unlocked())<'0'); for(x-='0'; '0'<=(_=getchar_unlocked()); x=(x<<3)+(x<<1)+_-'0');}while(0)
 char _;
+
+// scanner using pointers (fastest in this code)
+#define bs 1<<20 // buffer size
+char buf[bs];
+char *ptr = buf;
+void buff(){
+    fread(buf,1,bs,stdin);
+}
+long long fscan(){ // fast input template
+    long long num=0;
+    while(*ptr<'0'||*ptr>'9')++ptr; // Skip non-digit characters
+    while(*ptr>='0'&&*ptr<='9') {
+        num=num*10+(*ptr-'0');
+        ++ptr;
+    }
+    return num;
+}
 // method scanner
 int scan() {
     int num = 0;
@@ -23,8 +46,9 @@ int nscan() {
 }
 // reference
 int main(){
-int a = scan();
-int b = nscan();
+int a = fscan();
+int b = scan();
 int c;
 scan(c);
+int d=nscan();
 }
