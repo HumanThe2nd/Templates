c0c341ac3837887260ef32009d66f4ca754624ce
Dan Shan <96434604+HumanThe2nd@users.noreply.github.com>
2025-12-08 00:27:05 -0500

    Implement DSU structure and update author info
    Added a Disjoint Set Union (DSU) structure with union-find operations and updated the author information. GPT helped

--- disjoint_set.cpp
+++ disjoint_set.cpp
@@ -1,8 +1,25 @@
 /*
 C++ Disjoint set template
-Author: HumanThe2nd
+Author: Dan Shan
 Date: 2025-06-17
+Updated: 2025-08-2025
 */
+struct DSU { // Tracks size of sets (creds to GPT for help)
+    int n;
+    vector<int> p, sz;
+    DSU(int n=0): n(n), p(n), sz(n,1) { iota(p.begin(), p.end(), 0); }
+    int find(int a){ return p[a]==a ? a : p[a]=find(p[a]); }
+    bool unite(int a, int b){
+        a = find(a); b = find(b);
+        if(a==b) return false;
+        if(sz[a] < sz[b]) swap(a,b);
+        p[b] = a;
+        sz[a] += sz[b];
+        return true;
+    }
+    int size(int a){ return sz[find(a)]; }
+};
+
 class ds{
 	std::vector<int> p; // parent
 public:
@@ -26,7 +43,8 @@ public:
 int main(){
 	vector<ds> a(100); // making 100 dsets inside vector
 	for(int i=0;i<100;i++) a[i]=ds(1000);
-	
+
+	DSU dsu(n); // single dset
 	ds dset = ds(1000) // make a single dset
 	return 0;
 }
