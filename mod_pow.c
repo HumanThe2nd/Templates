e376d193f3c21446440429c1b521d192cab2df85
Dan Shan <96434604+HumanThe2nd@users.noreply.github.com>
2026-08-10 21:07:32 -0400

    Update mod_pow.c

--- mod_pow.c
+++ mod_pow.c
@@ -2,20 +2,20 @@
 Fast Modulus Exponents
 Author: HumanThe2nd
 Date: 2025-04-30
+Updated formatting: 2026-08-10
 Time Complexity: O(log(e))
 */
 #include <stdio.h>
 typedef long long ll;
-ll mod_pow(ll b,int e,ll mod){ // fast exponent with modulus
-    ll res=1;
-    b%=mod;
-    while(e>0) {
-        if (e&1)  // If exponent is odd
-            res=(res*b)%mod;
-
-        b=(b*b)%mod;
-        e>>=1;
+ll mod = 998244353;
+ll mpow(ll b, ll e){ // pow with mod 
+    ll res = 1;
+    b %= mod;
+    if(!b) return 0;
+    while(e){
+        if(e & 1) res = res * b % mod;
+        b = b * b % m;
+        e >>= 1;
     }
-
     return res;
 }
